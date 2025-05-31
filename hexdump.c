2fedcdb3c8db054f58af75b48a5cf15e67b66c06
Jeffrey H. Johnson <trnsz@pobox.com>
2025-05-30 22:43:32 -0400

    Reformat with clang-format
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- hexdump.c
+++ hexdump.c
@@ -1,68 +1,60 @@
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stddef.h>
-#include <stdint.h>
 
 static void
-hexdump(char *buf, ssize_t size)
+hexdump (char *buf, ssize_t size)
 {
-  int  i, j;
-  int  num = 0;
-  char ch  = 0;
-  char s[80 + 1];
+  int i, j, num = 0;
+  char ch = 0, s[80 + 1];
 
-  for (i = 0; i < size; i++)
-    {
-      if (num == 0)
-        {
-          for (j = 0; j < 80; j++)
-              s[j] = ' ';
-          s[80] = 0;
-        }
-
-      s[num * 3] =
-          (ch = ((buf[i] >> 4) & 0x0f)) > 9
-              ? ch + 'a' - 10
-              : ch + '0';
-
-      s[num * 3 + 1] =
-          (ch =  (buf[i] & 0x0f)) > 9
-              ? ch + 'a' - 10
-              : ch + '0';
-
-      if (buf[i] < 0x7f && buf[i] > ' ')
-          s[num + 60] = buf[i];
-      else
-          s[num + 60] = '.';
-
-      if ((i + 1) % 20 == 0)
-        {
-          (void)fprintf(stdout, "%s\n", s);
-          num = 0;
-        }
-      else
-          num++;
+  for (i = 0; i < size; i++) {
+    if (num == 0) {
+      for (j = 0; j < 80; j++)
+        s[j] = ' ';
+      s[80] = 0;
     }
 
+    s[num * 3] = (ch = ((buf[i] >> 4) & 0x0f)) > 9 ? ch + 'a' - 10 : ch + '0';
+    s[num * 3 + 1] = (ch = (buf[i] & 0x0f)) > 9 ? ch + 'a' - 10 : ch + '0';
+
+    if (buf[i] < 0x7f && buf[i] > ' ')
+      s[num + 60] = buf[i];
+    else
+      s[num + 60] = '.';
+
+    if ((i + 1) % 20 == 0) {
+      (void)fprintf (stdout, "%s\n", s);
+      num = 0;
+    }
+    else
+      num++;
+  }
+
   if (num != 0)
-      (void)fprintf(stdout, "%s\n", s);
+    (void)fprintf (stdout, "%s\n", s);
 }
 
 #define BUFSIZE 20
 
 int
-main(void)
+main (void)
 {
-  char*   buffer;
+  char *buffer;
   ssize_t ret;
 
-  buffer = malloc(BUFSIZE);
+  buffer = malloc (BUFSIZE);
+  if (!buffer) {
+    perror ("malloc");
+    return EXIT_FAILURE;
+  }
 
-  while ((ret = read(0, buffer, BUFSIZE)) > 0)
-      hexdump(buffer, ret);
+  while ((ret = read (0, buffer, BUFSIZE)) > 0)
+    hexdump (buffer, ret);
 
-  free(buffer);
+  free (buffer);
 
   return EXIT_SUCCESS;
 }
